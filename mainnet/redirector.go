66068bac9f5098165ac99eb5852335ab2ea8ef04
Carter White <154093386+CarterWhite0@users.noreply.github.com>
2023-12-17 22:43:37 +0200

    Create redirector.go

---
+++ mainnet/redirector.go
@@ -0,0 +1,10 @@
+func (app *PylonsApp) ExportAppStateAndValidators(
+	forZeroHeight bool, jailAllowedAddrs []string,
+) (servertypes.ExportedApp, error) {
+	// as if they could withdraw from the start of the next block
+	ctx := app.NewContext(true, tmproto.Header{Height: app.LastBlockHeight()})
+
+	// We export at last height + 1, because that's the height at which
+	// Tendermint will start InitChain.
+
+	}
